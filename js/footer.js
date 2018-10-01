40f08d7e09f171369fa2b4aa794d05c99e0a0b29
felipe <4037580@gmail.com>
2018-09-30 22:44:47 -0600

    first

---
+++ js/footer.js
@@ -0,0 +1,38 @@
+document.getElementById("footerScript").innerHTML =
+'<div class="container">'+
+  '<div class="row">'+
+
+    '<div class="col s12 m4 center">'+
+        '<h5 class="white-text">Email Us:</h5>'+
+          '<a href="mailto:contact@fianzaglobal.com" target="_blank" class="white-text">contact@fianzaglobal.com</a><br>'+
+        '<br>'+
+    '</div>'+
+
+    '<div class="col s12 m4 center">'+
+        '<h5 class="white-text">Call Us:</h5>'+
+        '<p class="white-text">'+
+          '<a href="tel:1-720-437-1072" class="white-text">720-437-1072</a><br>'+
+        '</p>'+
+        '<br>'+
+    '</div>'+
+
+    '<div class="col s12 m4 center">'+
+        '<h5 class="white-text">Follow Us:</h5>'+
+      '<p class="white-text">'+
+        '<a href="#!" target="_blank"><img src="img/social/linkedin.svg" title="Linkedin" alt="linkedin" class="social"></a>'+
+        '<a href="#!" target="_blank"><img src="img/social/facebook.svg" title="Facebook" alt="facebook" class="social"></a>'+
+        '<a href="#!" target="_blank"><img src="img/social/twitter.svg" title="Twitter" alt="twitter" class="social"></a>'+
+        '<a href="#!" target="_blank"><img src="img/social/youtube.svg" title="YouTube" alt="youtube" class="social"></a>'+
+        '<a href="#!" target="_blank"><img src="img/social/instagram.svg" title="Instagram" alt="instagram" class="social"></a>'+
+      '</p>'+
+    '</div>'+
+
+  '</div>'+
+'</div><br>'+
+'<div class="footer-copyright">'+
+  '<div class="container center">'+
+  '<a class="green-text" href="#!">Disclaimer</a> &nbsp;&nbsp; | &nbsp;&nbsp; <a class="green-text" href="#!">Privacy Policy</a> &nbsp;&nbsp; | &nbsp;&nbsp; © Copyright '+
+  new Date().getFullYear()+
+  ' <a class="white-text" href="#!">Fianza, LLC. All rights reserved.</a>'+
+  '</div>'+
+'</div>';
---
+++ js/nav.js
@@ -0,0 +1,23 @@
+document.getElementById("navScript").innerHTML =
+  '<div class="nav-wrapper container">'+
+    '<a id="logo-container" href="/" class="brand-logo"><img src="fianzalogo6.svg" class="fianza-logo"></a>'+
+    '<ul class="right hide-on-med-and-down">'+
+      '<li><a href="/">Home</a></li>'+
+      '<li><a href="services.html">Services</a></li>'+
+      '<li><a href="about.html">About Us</a></li>'+
+      '<li><a href="contact.html">Contact Us</a></li>'+
+    '</ul>'+
+    '<ul class="left hide-on-med-and-down">'+
+      '<li class="slogan"><a href="/" style="font-size: 12px; margin: -4px 0 0 160px;"><em>Developing greater financial confidence in your business</em></a></li>'+
+    '</ul>'+
+
+    '<ul id="nav-mobile" class="side-nav">'+
+      '<li><a href="/"><i class="material-icons blue-text">home</i>Home</a></li>'+
+      '<li><a href="services.html"><i class="material-icons blue-text">explore</i>Services</a></li>'+
+      '<li><a href="about.html"><i class="material-icons blue-text">check_circle</i>About Us</a></li>'+
+      '<li><a href="contact.html"><i class="material-icons blue-text">info</i>Contact Us</a></li>'+
+      '<li><a href="mailto:contact@fianzaglobal.com" target="_blank" class="green-text"><i class="material-icons green-text">email</i>contact@fianzaglobal.com</a></li>'+
+      '<li><a href="tel:1-720-437-1072" class="green-text"><i class="material-icons green-text">phone</i>720-437-1072</a></li>'+
+    '</ul>'+
+    '<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>'+
+  '</div>';
